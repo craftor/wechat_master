aff6cbcf8145c4065bd174a6b86c15fbcfd5c175
Craftor <craftor@126.com>
2018-04-14 20:24:37 +0800

    upup

--- WeChatBase.py
+++ WeChatBase.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
-import os, time
-import cv2
+import datetime
+import os
+import time
 import numpy as np
-from matplotlib import pyplot as plt
-
-# 软件版本
-VERSION = "0.1"
+import cv2
+import subprocess
+from threading import Thread
+from multiprocessing import Process
+from AndroidBase import AndroidBase
 
 # 分辨率
 resolution = "1080"  # 1080 or 720
 # 图标存放目标
-dir_root = "template"
+dir_root = "wechat"
 # 图标名
 main_tongxunlu = 'main_tongxunlu.png' # 主菜单，通信录
 main_faxian    = 'main_faxian.png'    # 主菜单，发现
@@ -24,85 +26,24 @@ zan_fasong     = 'zan_fasong.png'     # 点赞，发送
 pyq_fanhui     = 'pyq_fanhui.png'     # 朋友圈，返回键图标
 pyq_text       = 'pyq_text.png'       # 朋友圈，文字
 pyq_xiangji    = 'pyq_xiangji.png'    # 朋友圈，相机图标
-# 截图文件名
-ScreenShotFileName = "Tmp01.png"
-ScreenShotDetected = "Tmp02.png"
 
 class WeChatBase():
 
     # 启动初始化
     def __init__(self):
-        self.width = 1080  # 默认屏幕宽
-        self.height = 1920 # 默认屏幕高
-        self.threshold = 0.7 # 图像比对默认阈值
-        self.GetScreenSize() # 初始化先通过截图试获取屏幕分辨率
-
-    # 打印消息
-    def mylog(self, text):
-        print(text)
-
-    # 截屏
-    def PullScreenShot(self):
-        os.system('adb shell screencap -p /sdcard/' + ScreenShotFileName)
-        os.system('adb pull /sdcard/' + ScreenShotFileName + ' .')
-
-    # 延时，时间单位为秒
-    def Sleep(self, t):
-        time.sleep(t)
-
-    # 单击操作
-    def OneClick(self, x, y):
-        os.system('adb shell input tap ' + str(x) + ' ' + str(y))
-
-    # 双击操作
-    def DoubleClock(self, x, y):
-        pass
-
-    # 返回按钮
-    def ClickReturn(self):
-        os.system('adb shell input keyevent 4')
-
-     # 滑动操作
-    def Rolling(self, x1, y1, x2, y2):
-        os.system('adb shell input swipe ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2))
-
-    # 上滑屏幕
-    def RollingUpScreen(self, step):
-        self.mylog("屏幕上滑 " + str(step) + " pix")
-        self.Rolling(int(self.width/2), int(self.height/2), int(self.width/2), int(self.height/2) - step)
-
-    # 下滑屏幕
-    def RollingDownScreen(self, step):
-        self.mylog("屏幕下滑 " + str(step) + " pix")
-        self.Rolling(int(self.width/2), int(self.height/2), int(self.width/2), int(self.height/2) + step)
-
-    # 获取屏幕尺寸，非常重要
-    def GetScreenSize(self):
-        self.PullScreenShot()
-        img = cv2.imread(ScreenShotFileName, 3)
-        self.height, self.width = img.shape[:2]
-
-    # 点击电源键，点亮屏幕
-    def LightScreen(self):
-        os.system('adb shell input keyevent 26')
-
-    # 解锁屏幕
-    def UnlockScreen(self, phone):
-        self.LightScreen()
-        self.Sleep(1)
-        # 根据phone类型，选择滑动解锁方式
-        if phone == 0: # 上滑
-            self.RollingUpScreen(800)
+        self.myClient = AndroidBase()
+        self.myClient.dir_root = dir_root + "/" + resolution# 指定图标存放目录
 
     # 启动微信
     def LaunchWeChat(self):
-        os.system('adb shell am start com.tencent.mm/com.tencent.mm.ui.LauncherUI')
+        cmd = 'adb shell am start com.tencent.mm/com.tencent.mm.ui.LauncherUI'
+        self.myClient.SendCommand(cmd)
 
     # 点击“发现” -> "朋友圈"
     def EnterMoment(self):
-        self.OneClick(int(self.width*5/8), int(self.height*19/20))
-        self.Sleep(1)
-        self.OneClick(int(self.width*5/8), int(self.height*1/5))
+        self.myClient.OneClick(int(self.width*5/8), int(self.height*19/20))
+        self.myClient.Sleep(1)
+        self.myClient.OneClick(int(self.width*5/8), int(self.height*1/5))
 
     # 重启微信
     def ReLaunchWechat(self):
@@ -115,132 +56,77 @@ class WeChatBase():
         self.ClickReturn()
         self.LaunchWeChat()
 
-    # 找匹配图标
-    def MatchImg(self, Target):
-        # 原始图片
-        img_rgb = cv2.imread(ScreenShotFileName)
-        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-        # 比对模板图片
-        temp_url = dir_root + "/" + resolution + "/" + Target
-        #print(temp_url)
-        template = cv2.imread(temp_url, 0)
-        # 获取模板图片尺寸
-        w, h = template.shape[::-1]
-        # 比对操作
-        res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-        # 比对结果坐标
-        loc = np.where( res >= self.threshold)
-        #min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res) # 找到最大值和最小值
-        #print(cv2.minMaxLoc(res))
-        #print(loc)
-
-        # 描绘出外框
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (7,249,151), 2)
-        # 保存识别目标后的图
-        cv2.imwrite(ScreenShotDetected, img_rgb)
-
-        # 检查比对结果
-        for pp in loc:
-            # 如果不为空，说明有比对成果的内容
-            if len(pp) :
-                #print ("Yes")
-                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res) # 找到最大值和最小值
-                #print (max_loc)
-                return True, max_loc
-            else:
-                #print ("Empty")
-                return False, []
-
-    # For Test
-    def Test(self):
-        mylog("Test")
-
     # 检查是否在主菜单下
     def CheckIsMain(self):
-        # 比对条件
-        yes1, max_loc1 = self.MatchImg(main_tongxunlu)
-        yes2, max_loc2 = self.MatchImg(main_faxian)
-        yes3, max_loc3 = self.MatchImg(main_me)
-        # 检查
-        if (yes1 and yes2 and yes3):
-            return True
-        else:
-            return False
+        result = self.myClient.CompareThree(main_tongxunlu, main_faxian, main_me)
+        return result
 
     # 检查是否在朋友圈状态下
     def CheckInMoment(self):
-        # 比对条件
-        yes1, max_loc1 = self.MatchImg(pyq_fanhui)
-        yes2, max_loc2 = self.MatchImg(pyq_text)
-        yes3, max_loc3 = self.MatchImg(pyq_xiangji)
-        # 检查
-        if (yes1 and yes2 and yes3):
-            return True
-        else:
-            return False
+        result = self.myClient.CompareThree(pyq_fanhui, pyq_text, pyq_xiangji)
+        return result
 
     # 点赞
     def ClickLike(self):
 
         # 截图
-        self.PullScreenShot()
+        self.myClient.PullScreenShot()
 
-        # 检查是否在朋友圈下
-        if (self.CheckInMoment()):
-            self.mylog(u"当前在朋友圈下")
-        else:
-            self.mylog(u"不在朋友圈下")
-            self.Sleep(0.3)
-            self.mylog(u"检查是否在微信界面")
-            if (self.CheckIsMain()):
-                self.mylog(u"当前在微信主界面，准备切换到朋友圈界面")
-                self.EnterMoment()
-                # 截图检查
-                self.PullScreenShot()
-                if (self.CheckInMoment()):
-                    self.mylog(u"已经切换到朋友圈下")
-            else:
-                self.mylog(u"不在微信界面下")
-                # 启动微信
-                self.mylog(u"正在启动微信")
-                self.LaunchWeChat()
-                # 截图检查
-                self.PullScreenShot()
-                if (self.CheckIsMain()):
-                    self.mylog(u"微信启动成功")
-                    self.mylog(u"正在切换到朋友圈")
-                    self.EnterMoment()
-                    # 截图检查
-                    self.PullScreenShot()
-                    if (self.CheckInMoment()):
-                        self.mylog(u"已经切换到朋友圈下")
+        # # 检查是否在朋友圈下
+        # if (self.CheckInMoment()):
+        #     self.LogPrint(u"当前在朋友圈下")
+        # else:
+        #     self.LogPrint(u"不在朋友圈下")
+        #     self.Sleep(0.3)
+        #     self.LogPrint(u"检查是否在微信界面")
+        #     if (self.CheckIsMain()):
+        #         self.LogPrint(u"当前在微信主界面，准备切换到朋友圈界面")
+        #         self.EnterMoment()
+        #         # 截图检查
+        #         self.PullScreenShot()
+        #         if (self.CheckInMoment()):
+        #             self.LogPrint(u"已经切换到朋友圈下")
+        #     else:
+        #         self.LogPrint(u"不在微信界面下")
+        #         # 启动微信
+        #         self.LogPrint(u"正在启动微信")
+        #         self.LaunchWeChat()
+        #         # 截图检查
+        #         self.PullScreenShot()
+        #         if (self.CheckIsMain()):
+        #             self.LogPrint(u"微信启动成功")
+        #             self.LogPrint(u"正在切换到朋友圈")
+        #             self.EnterMoment()
+        #             # 截图检查
+        #             self.PullScreenShot()
+        #             if (self.CheckInMoment()):
+        #                 self.LogPrint(u"已经切换到朋友圈下")
 
         # 查找待点赞的朋友圈
-        yes1, max_loc1 = self.MatchImg(zan_rukou)
+        yes1, max_loc1 = self.myClient.MatchImg(zan_rukou)
 
         if yes1:
-            self.mylog(u"找到朋友圈消息，坐标：" + str(max_loc1))
+            print(u"找到朋友圈消息，坐标：" + str(max_loc1))
             # 点开赞框
-            self.mylog(u"查看是否赞过")
-            self.OneClick(max_loc1[0]+10, max_loc1[1]+10)
-            self.Sleep(1)
-            self.PullScreenShot()
-            yes2, max_loc2 = self.MatchImg(zan_text)
-            yes3, max_loc3 = self.MatchImg(zan_quxiao)
+            print(u"查看是否赞过")
+            self.myClient.OneClick(max_loc1[0]+10, max_loc1[1]+10)
+            self.myClient.Sleep(1)
+            self.myClient.PullScreenShot()
+            yes2, max_loc2 = self.myClient.MatchImg(zan_text)
+            yes3, max_loc3 = self.myClient.MatchImg(zan_quxiao)
 
             # 查看是否赞过
             if yes2 :
-                self.mylog(u"未赞过，准备点赞")
-                self.OneClick(max_loc2[0]+10, max_loc2[1]+10)
-                self.mylog(u"点赞成功")
+                print(u"未赞过，准备点赞")
+                self.myClient.OneClick(max_loc2[0]+10, max_loc2[1]+10)
+                print(u"点赞成功")
             #取消赞操作
             # elif yes3:
-            #     mylog(u"已经赞过，取消")
+            #     LogPrint(u"已经赞过，取消")
             #     self.OneClick(max_loc2[0]+10, max_loc2[1]+10)
             # 忽略
             else :
-                self.mylog(u"已经赞过，忽略")
-                self.OneClick(max_loc1[0]+10, max_loc1[1]+10)
+                print(u"已经赞过，忽略")
+                self.myClient.OneClick(max_loc1[0]+10, max_loc1[1]+10)
         else:
-            self.mylog(u"未发现最新朋友圈消息")
+            print(u"未发现最新朋友圈消息")
--- main.py
+++ main.py
@@ -1,25 +1,115 @@
 # -*- coding: utf-8 -*-
-import os
-import time
-import numpy as np
-import cv2
-import WeChatBase
 
-VERSION = "0.1"
+"""
+Module implementing Form.
+"""
 
-def showImg(res):
-    cv2.namedWindow('Test', cv2.WINDOW_KEEPRATIO)
-    cv2.imshow('Test',res)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QObject
+
+from Ui_main import Ui_Dialog
+
+import datetime,sys
+from threading import Thread
+
+from WeChatBase import WeChatBase
+from AndroidBase import AndroidBase
+
+# 输出重定向
+class EmittingStream(QObject):
+    textWritten = pyqtSignal(str)
+    def write(self, text):
+        self.textWritten.emit(str(text))
+
+class Main(QWidget, Ui_Dialog):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent=None):
+        """
+        Constructor
+
+        @param parent reference to the parent widget
+        @type QWidget
+        """
+        # 微信
+        self.myWeChat = WeChatBase()
+        self.myAndroid = AndroidBase()
+
+        # 点赞线程
+        self.t_LikeRun = False
+
+        # 重定向
+        sys.stdout = EmittingStream(textWritten=self.LogPrint)
+        sys.stderr = EmittingStream(textWritten=self.LogPrint)
+
+        super(Main, self).__init__(parent)
+        self.setupUi(self)
+
+    # 打印日志
+    def LogPrint(self, text):
+        now = datetime.datetime.now()
+        otherStyleTime = now.strftime("%Y-%m-%d %H:%M:%S")
+        mystr = otherStyleTime + "  " + text
+        self.textEdit_Print.append(mystr)
+
+    # 朋友圈线程入口
+    def ThreadClickLike(self):
+        while(self.t_LikeRun):
+            #self.LogPrint(u"点赞开始")
+            self.myWeChat.ClickLike()
+            print(u"上滑屏幕")
+            self.myAndroid.RollingUpScreen(500)
+            #self.Sleep(0.5)
+            #self.LogPrint(u"点赞结束")
+
+    @pyqtSlot()
+    def on_pushButton_AutoStartWeChat_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        if (self.t_LikeRun):
+            self.pushButton_AutoStartWeChat.setText(u"停止中。。。")
+            print(u"==========正在退出点赞线程==========")
+            self.t_LikeRun = False
+            self.t_LikeThread.join()
+            print(u"==========点赞线程已经停止==========")
+            self.pushButton_AutoStartWeChat.setText(u"开始")
+        else:
+            print(u"==========正在启动点赞线程==========")
+            self.t_LikeRun = True
+            self.t_LikeThread = Thread(target=self.ThreadClickLike, name='LikeThread')
+            self.t_LikeThread.start()
+            print(u"==========点赞线程已经启动==========")
+            self.pushButton_AutoStartWeChat.setText(u"停止")
+
+    @pyqtSlot()
+    def on_pushButton_RunWeChat_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        pass
+
+    @pyqtSlot()
+    def on_pushButton_ClearLog_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        self.textEdit_Print.setText("")
+
+    @pyqtSlot()
+    def on_pushButton_AutoAddFriends_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        pass
 
 if __name__ == '__main__':
-    myWeChat = WeChatBase.WeChatBase()
-
-    #InterWeChatMoment()
-    #myWeChat.CheckInMoment()
-
-    while(True):
-        myWeChat.ClickLike()
-        myWeChat.RollingUpScreen(500)
-        myWeChat.Sleep(0.5)
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    #app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    dlg = Main()
+    dlg.show()
+    sys.exit(app.exec_())
